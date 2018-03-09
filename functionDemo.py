88d69e94e16b785e7f62cc9d6ae999cf94ad9480
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-09 08:28:46 -0500

    Update functionDemo.py

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,34 @@
+#IanNolon
+#3/9/18
+#functionDemo.py - how to write our own functions
 
+def hw():
+    print("hello, world")
+    
+hw() #test of hello world function
+hw() #another one
+    
+def double(thingToDouble):
+    print(thingToDouble*2)
+
+double(12) #test of double function
+double('w') #test of double with a string input
+double(False) #test of double with a boolean
+
+def bigger(a,b):
+    if a>b:
+        print(a)
+    elif b>a:
+        print(b)
+    else:
+        print(a)
+        
+bigger(3,4)
+bigger('Ian','Nolon') #later in the alphabet means bigger, not how many characters it has
+bigger(True,False)
+
+def slope(x1, y1, x2, y2):
+    print((y2 - y1)/(x2 - x1))
+    
+slope(1,1,2,2)
+slope(True,True,False,False)
