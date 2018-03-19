1ca0025b5bae3bf6a1d151954640528c5f058511
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-19 08:47:04 -0400

    Create functionNotes.py

---
+++ functionNotes.py
@@ -0,0 +1,27 @@
+#IanNolon
+#3/20/18
+#functionNotes.py
+
+'''
+Python Built-in functions
+
+Max( , )
+Min( , )
+.upper( )
+.lower( )
+print( )
+randint( , )
+split( )
+str( )
+int( )
+float( )
+range( , )
+len( )
+input( )
+.count( )
+
+Writing our own functions
+
+def functionName(a,b,c,...): #a,b,and c are arguments/placeholders for user input
+    #lines that are executed when the function is called
+    return Value (optional)
