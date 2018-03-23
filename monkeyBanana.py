226652719d26f1f76b8a3eea5c1d46fbf057bd1a
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-23 08:49:47 -0400

    Update monkeyBanana.py

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -9,69 +9,86 @@ from random import randint
 ROWS = 20
 COLS = 40
 CELL_SIZE = 20
+BANANA_SPEED = 180
 
+#move monkey right one cell if possible
 def moveRight(event):
     if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
     
+#move monkey left one cell if possible
 def moveLeft(event):
     if monkey.x > 0:
         monkey.x -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
     
+#move monkey up one cell if possible
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
     
+#move monkey down one cell if possible
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
     
+#move banana to a random location and reset timer
 def moveBanana():
+    data['frames'] = 0 
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
     
-    
-    
+#increase score and display new text at bottom of screen
+def updateScore():
+    data['score'] += 10
+    data['ScoreText'].destroy() #remove old writing
+    scoreBox = TextAsset('Score = '+str(data['score']))
+    data['ScoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE+10))
 
+#keeps track of how many frames have happened and moves banana if it has been
+#more than 300 frames since last move
+def step():
+    data['frames'] +=1
+    if data['frames'] == BANANA_SPEED:
+        moveBanana()
+
+#sets up and runs the game
 if __name__ == '__main__':
     
     #hold variables in a dictionary
     data = {}
     data['score'] = 0
+    data['frames'] = 0
     
     #colors
     lakeColor = Color(0x0000FF,1)
-    monkeyColor = Color(0x8B4513,1)
-    bananaColor = Color(0xFFFF00,1)
+    monkeyColor = Color(0x08E7AA,1)
+    bananaColor = Color(0x52470B,1)
     
+    #graphics
     lakeBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,lakeColor),lakeColor)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,monkeyColor),monkeyColor)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,bananaColor),bananaColor)
-    scoreText = TextAsset(('Score =',data['score']), fill = bananaColor, style = 'bold 20pt Times')
+    scoreBox = TextAsset('Score = 0')
     
     Sprite(lakeBox)
     monkey = Sprite(monkeyBox)
     banana = Sprite(bananaBox,(CELL_SIZE*COLS/2,CELL_SIZE*ROWS/2))
-    scoreBox = Sprite(scoreText,(0,ROWS*CELL_SIZE+10))
+    data['ScoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE+10))
     
     App().listenKeyEvent('keydown','right arrow',moveRight)
     App().listenKeyEvent('keydown','left arrow',moveLeft)
     App().listenKeyEvent('keydown','up arrow',moveUp)
     App().listenKeyEvent('keydown','down arrow',moveDown)
-    App().run()
+    App().run(step)
