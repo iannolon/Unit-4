4954fee550388da57ab2b103b159095691a8588a
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-26 08:29:56 -0400

    Update colorChangeWindow.py

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,21 @@
+#IanNolon
+#3/26/18
+#colorChangeWindow.py
 
+from ggame import *
+
+black = Color(0x000000,1)
+aqua = Color(0x00FFFF,1)
+gray = Color(0x808080,1)
+navy = Color(0x000080,1)
+silver = Color(0xC0C0C0,1)
+green = Color(0x008000,1)
+olive = Color(0x808000,1)
+teal = Color(0x008080,1)
+blue = Color(0x0000FF,1)
+lime = Color(0x00FF00,1)
+purple = Color(0x800080,1)
+white = Color(0xFFFFFF,1)
+maroon = Color(0x800000,1)
+red = Color(0xFF0000,1)
+yellow = Color(0xFFFF00,1)
