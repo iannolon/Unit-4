80be476653eae4ab59a10edc3943315a98db17bf
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-26 19:02:16 -0400

    Update and rename printStars.py to printSquares.py

---
+++ printSquares.py
@@ -0,0 +1,9 @@
+#IanNolon
+#3/26/18
+#printSquares.py
+
+
+def printSquares(row,col):
+    print('+','--+'*col)
+    print('|','  |'*col)
+printSquares(10,5)
--- printStars.py
+++
@@ -1,7 +0,0 @@
-#IanNolon
-#3/26/18
-#printSquares.py
-
-col = 5
-for c in range (1,col):
-    print('+--'*c)
