0be8949d2d3a63bd0f8e9af0228b649064f3d411
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-26 08:23:41 -0400

    Update warmup11.py

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,14 @@
+#IanNolon
+#3/26/18
+#warmup11.py - returns True if a number is prime and False otherwise
 
+def prime(num):
+    a=0 #a is just a variable to store True/False data outside of the for loop
+    for i in range (2,num):
+        if num % i == 0:
+            a=1
+    if a == 1 or num <= 1:
+        return False
+    else:
+        return True
+print(prime(3))
