7d09127bac1c6230b2b54ac6861b559d88a843a8
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-29 08:21:26 -0400

    Update warmup12.py

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,11 @@
+#IanNolon
+#3/28/18
+#warmup12.py - write a function that returns the GCF of two numbers
+
+def GCF(a,b):
+    for i in range(a+1,0,-1):
+        if a%i == 0:
+            if b%i == 0:
+                return i
+print(GCF(16,64))
 
