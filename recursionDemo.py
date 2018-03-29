eeeaff8f4e8aef270b60d9ed4c59e740f1fe2435
iannolon <35769152+iannolon@users.noreply.github.com>
2018-03-29 08:07:07 -0400

    Update recursionDemo.py

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,1 +1,11 @@
+#IanNolon
+#3/28/18
+#recursionDemo.py - recursve version of countdown
 
+def countdownr(n):
+    if n == 0: #base case
+        print('BOOM!')
+    else: #recursive step
+        print(n)
+        countdownr(n-1)
+countdownr(5)
