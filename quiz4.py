b6860c5f81b36f9d22c1b9e6e769513270ef8f4b
iannolon <35769152+iannolon@users.noreply.github.com>
2018-04-02 08:25:13 -0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,23 @@
+#IanNolon
+#4/2/18
+#quiz4.py
 
+def count(num):
+    for i in range (1,num+1):
+        print(i)
+def excitedPrint(sen):
+    print(sen.upper(),'!!!')
+def firstLetter(string):
+    for ch in string:
+        return (ch)
+        break
+def repeats(n1,n2,n3):
+    if n1 == n2 or n1 == n3 or n2 == n3:
+        return(True)
+    else:
+        return(False)
+
+count(7)
+excitedPrint('I <3 Programming')
+print(firstLetter('Smedinghoff'))
+print(repeats(5,6,5))
